81444a6bd69516c186fb741fd019b25aca3b05f3
TheBilo16 <58870674+TheBilo16@users.noreply.github.com>
2020-01-03 13:51:41 -0500

    Image expand

--- js/all.js
+++ js/all.js
@@ -37,6 +37,74 @@ var slider = {
    }
 }
 
+//Images View
+var viewImages = {
+   translate_x : 0,
+   divHide : () => document.querySelector("#content-images-hide"),
+   contentHide : () => document.querySelector("#content-images-slider"),
+   imagesHome : () => document.querySelectorAll(".content-images .content .img"),
+   listImagesHide : function(){
+      return this.contentHide().querySelectorAll(".image-expand");
+   },
+   buttons : id => document.querySelector(`.view-images-expand #${id}`),
+   __init__ : function(){
+      let template = "";
+      for(let img of this.imagesHome()){
+         template += `<img src="${img.src}" alt="image-hide" class="image-expand">`; 
+      }
+      this.contentHide().innerHTML = template;
+      this.slide();
+      this.clickImage();
+      this.close();
+   },
+   slide : function(){
+      let images = this.listImagesHide();
+      let left = this.buttons("btn-left");
+      let right = this.buttons("btn-right");
+
+      right.addEventListener("click", ev => {
+         let maxTranslate = 100 * (images.length - 1);      
+
+         if(this.translate_x < maxTranslate)
+            this.translate_x += 100;
+
+         //Translate images
+         for(let img of images){
+            img.style.transform = `translateX(-${this.translate_x}%)`;
+         }
+      })
+
+      left.addEventListener("click", ev => {
+         if(this.translate_x > 0)
+            this.translate_x -= 100;
+
+         //Translate images
+         for(let img of images){
+            img.style.transform = `translateX(-${this.translate_x}%)`;
+         }
+      })
+
+   },
+   clickImage : function(){
+      let images = document.querySelectorAll(".content-images .content .icon");
+      [...images].forEach((img,i) => {
+         img.addEventListener("click", ev => {
+            this.translate_x = 100 * i;
+            for(let imgh of this.listImagesHide()){
+               imgh.style.transform = `translateX(-${this.translate_x}%)`;
+            }
+            this.divHide().classList.add("animate");
+         });
+      });
+   },
+   close : function(){
+      let close = this.buttons("btn-close");
+      close.addEventListener("click", ev => {
+         this.divHide().classList.remove("animate");
+      });
+   }
+}
+
 var floatingContent = {
    
 }
--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
 window.onload = function(){
    slider.init_slider();
+   viewImages.__init__();
 }
